248063ca4d0c4b0c5edbbb99581a71cb8e3a5b1c
Mikolaj Przybylski <36564831+0GreenClover0@users.noreply.github.com>
2022-02-11 22:16:43 +0100

    Create image views

--- VulkanEngine.cpp
+++ VulkanEngine.cpp
@@ -43,6 +43,7 @@ private:
 	VkExtent2D swap_chain_extent;
 
 	std::vector<VkImage> swap_chain_images;
+	std::vector<VkImageView> swap_chain_image_views;
 
 	const std::vector<const char*> validation_layers = { "VK_LAYER_KHRONOS_validation" };
 	const std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
@@ -78,6 +79,7 @@ private:
 		pick_physical_device();
 		create_logical_device();
 		create_swap_chain();
+		create_image_views();
 	}
 
 	void main_loop()
@@ -468,8 +470,37 @@ private:
 		swap_chain_extent = extent;
 	}
 
+	void create_image_views()
+	{
+		swap_chain_image_views.resize(swap_chain_images.size());
+
+		for (int i = 0; i < swap_chain_images.size(); i++)
+		{
+			VkImageViewCreateInfo create_info{};
+			create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			create_info.image = swap_chain_images[i];
+			create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			create_info.format = swap_chain_image_format;
+			create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			create_info.subresourceRange.baseMipLevel = 0;
+			create_info.subresourceRange.levelCount = 1;
+			create_info.subresourceRange.baseArrayLayer = 0;
+			create_info.subresourceRange.layerCount = 0;
+
+			if (vkCreateImageView(device, &create_info, nullptr, &swap_chain_image_views[i]) != VK_SUCCESS)
+				throw std::runtime_error("Failed to create an image view");
+		}
+	}
+
 	void cleanup()
 	{
+		for (auto image_view : swap_chain_image_views)
+			vkDestroyImageView(device, image_view, nullptr);
+
 		vkDestroySwapchainKHR(device, swap_chain, nullptr);
 
 		vkDestroyDevice(device, nullptr);
