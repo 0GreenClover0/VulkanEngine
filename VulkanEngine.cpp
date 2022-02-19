ab5e529b55c247bf1254037136d405df50bed9fc
Mikolaj Przybylski <36564831+0GreenClover0@users.noreply.github.com>
2022-02-19 11:26:22 +0100

    Texture loading and creating an image for it
    We are doing this by copying data from the buffer to an image.

--- VulkanEngine.cpp
+++ VulkanEngine.cpp
@@ -7,6 +7,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <algorithm>
 #include <array>
 #include <chrono>
@@ -103,8 +106,10 @@ private:
 	VkBuffer index_buffer;
 	VkDeviceMemory index_buffer_memory;
 	VkDescriptorPool descriptor_pool;
-	std::vector<VkDescriptorSet> descriptor_sets;
+	VkImage texture_image;
+	VkDeviceMemory texture_image_memory;
 
+	std::vector<VkDescriptorSet> descriptor_sets;
 	std::vector<VkBuffer> uniform_buffers;
 	std::vector<VkDeviceMemory> uniform_buffers_memory;
 	std::vector<VkSemaphore> image_available_semaphores;
@@ -172,6 +177,7 @@ private:
 		create_graphics_pipeline();
 		create_framebuffers();
 		create_command_pool();
+		create_texture_image();
 		create_vertex_buffer();
 		create_index_buffer();
 		create_uniform_buffers();
@@ -909,6 +915,85 @@ private:
 			throw std::runtime_error("Failed to create command pool.");
 	}
 
+	void create_image(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, 
+					  VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& image_memory)
+	{
+		// https://vulkan-tutorial.com/Texture_mapping/Images#page_Staging-buffer
+		VkImageCreateInfo image_info{};
+		image_info.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		image_info.imageType = VK_IMAGE_TYPE_2D;
+		image_info.extent.width = width;
+		image_info.extent.height = height;
+		image_info.extent.depth = 1;
+		image_info.mipLevels = 1;
+		image_info.arrayLayers = 1;
+		image_info.format = format;
+		image_info.tiling = tiling;
+		image_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		image_info.usage = usage;
+		image_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		image_info.samples = VK_SAMPLE_COUNT_1_BIT;
+		image_info.flags = 0;
+
+		if (vkCreateImage(device, &image_info, nullptr, &image) != VK_SUCCESS)
+			throw std::runtime_error("Failed to create an image.");
+
+		VkMemoryRequirements mem_requirements;
+		vkGetImageMemoryRequirements(device, image, &mem_requirements);
+
+		VkMemoryAllocateInfo alloc_info{};
+		alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		alloc_info.allocationSize = mem_requirements.size;
+		alloc_info.memoryTypeIndex = find_memory_type(mem_requirements.memoryTypeBits, properties);
+
+		if (vkAllocateMemory(device, &alloc_info, nullptr, &image_memory) != VK_SUCCESS)
+			throw std::runtime_error("Failed to allocate image memory.");
+
+		vkBindImageMemory(device, image, image_memory, 0);
+	}
+
+	void create_texture_image()
+	{
+		// https://vulkan-tutorial.com/Texture_mapping/Images#page_Staging-buffer
+		int tex_width, tex_height, tex_channels;
+		// FIXME: don't hardcode this
+		stbi_uc* pixels = stbi_load("textures/texture.jpg", &tex_width, &tex_height, &tex_channels, STBI_rgb_alpha);
+
+		VkDeviceSize image_size = tex_width * tex_height * 4;
+
+		if (!pixels)
+			throw std::runtime_error("Failed to load texture image.");
+
+		VkBuffer staging_buffer;
+		VkDeviceMemory staging_buffer_memory;
+
+		create_buffer(image_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, staging_buffer, staging_buffer_memory);
+
+		void *data;
+		vkMapMemory(device, staging_buffer_memory, 0, image_size, 0, &data);
+		memcpy(data, pixels, static_cast<size_t>(image_size));
+		vkUnmapMemory(device, staging_buffer_memory);
+
+		stbi_image_free(pixels);
+
+		create_image(tex_width, tex_height,
+		VK_FORMAT_R8G8B8A8_SRGB,
+		VK_IMAGE_TILING_OPTIMAL,
+		VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+		texture_image,
+		texture_image_memory);
+
+		transition_image_layout(texture_image, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+
+		copy_buffer_to_image(staging_buffer, texture_image, static_cast<uint32_t>(tex_width), static_cast<uint32_t>(tex_height));
+
+		transition_image_layout(texture_image, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+		vkDestroyBuffer(device, staging_buffer, nullptr);
+		vkFreeMemory(device, staging_buffer_memory, nullptr);
+	}
+
 	// https://vulkan-tutorial.com/en/Vertex_buffers/Vertex_buffer_creation#page_Buffer-creation
 	void create_vertex_buffer()
 	{
@@ -1005,12 +1090,8 @@ private:
 		vkBindBufferMemory(device, buffer, buffer_memory, 0);
 	}
 
-	void copy_buffer(VkBuffer src_buffer, VkBuffer dst_buffer, VkDeviceSize size)
+	VkCommandBuffer begin_single_time_commands()
 	{
-		/* TODO: You may wish to create a separate command pool for these kinds of short-lived buffers,
-		because the implementation may be able to apply memory allocation optimizations.
-		You should use the VK_COMMAND_POOL_CREATE_TRANSIENT_BIT flag during command pool generation in that case.*/
-
 		VkCommandBufferAllocateInfo alloc_info{};
 		alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
 		alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
@@ -1026,12 +1107,11 @@ private:
 
 		vkBeginCommandBuffer(command_buffer, &begin_info);
 
-		VkBufferCopy copy_region{};
-		copy_region.srcOffset = 0;
-		copy_region.dstOffset = 0;
-		copy_region.size = size;
-		vkCmdCopyBuffer(command_buffer, src_buffer, dst_buffer, 1, &copy_region);
+		return command_buffer;
+	}
 
+	void end_single_time_commands(VkCommandBuffer command_buffer)
+	{
 		vkEndCommandBuffer(command_buffer);
 
 		VkSubmitInfo submit_info{};
@@ -1049,6 +1129,101 @@ private:
 		vkQueueWaitIdle(graphics_queue);
 
 		vkFreeCommandBuffers(device, command_pool, 1, &command_buffer);
+	}
+
+	void copy_buffer(VkBuffer src_buffer, VkBuffer dst_buffer, VkDeviceSize size)
+	{
+		/* TODO: You may wish to create a separate command pool for these kinds of short-lived buffers,
+		because the implementation may be able to apply memory allocation optimizations.
+		You should use the VK_COMMAND_POOL_CREATE_TRANSIENT_BIT flag during command pool generation in that case.*/
+
+		VkCommandBuffer command_buffer = begin_single_time_commands();
+
+		VkBufferCopy copy_region{};
+		copy_region.srcOffset = 0;
+		copy_region.dstOffset = 0;
+		copy_region.size = size;
+		vkCmdCopyBuffer(command_buffer, src_buffer, dst_buffer, 1, &copy_region);
+
+		end_single_time_commands(command_buffer);
+	}
+
+	// https://vulkan-tutorial.com/Texture_mapping/Images#page_Copying-buffer-to-image
+	void copy_buffer_to_image(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height)
+	{
+		VkCommandBuffer command_buffer = begin_single_time_commands();
+
+		VkBufferImageCopy region{};
+		region.bufferOffset = 0;
+		region.bufferRowLength = 0;
+		region.bufferImageHeight = 0;
+		region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		region.imageSubresource.mipLevel = 0;
+		region.imageSubresource.baseArrayLayer = 0;
+		region.imageSubresource.layerCount = 1;
+		region.imageOffset = {0, 0, 0};
+		region.imageExtent = {
+			width,
+			height,
+			1
+		};
+
+		vkCmdCopyBufferToImage(
+			command_buffer,
+			buffer,
+			image,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			1,
+			&region
+		);
+
+		end_single_time_commands(command_buffer);
+	}
+
+	void transition_image_layout(VkImage image, VkFormat format, VkImageLayout old_layout, VkImageLayout new_layout)
+	{
+		VkCommandBuffer command_buffer = begin_single_time_commands();
+
+		VkImageMemoryBarrier barrier{};
+		barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+		barrier.oldLayout = old_layout;
+		barrier.newLayout = new_layout;
+		barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+		barrier.image = image;
+		barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		barrier.subresourceRange.baseMipLevel = 0;
+		barrier.subresourceRange.levelCount = 1;
+		barrier.subresourceRange.baseArrayLayer = 0;
+		barrier.subresourceRange.layerCount = 1;
+
+		VkPipelineStageFlags source_stage;
+		VkPipelineStageFlags destination_stage;
+
+		if (old_layout == VK_IMAGE_LAYOUT_UNDEFINED && new_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+		{
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+			source_stage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+			destination_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+		}
+		else if (old_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && new_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+		{
+			barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			source_stage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+			destination_stage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		}
+		else
+		{
+			throw std::invalid_argument("Unsupported layer transition.");
+		}
+
+		vkCmdPipelineBarrier(command_buffer, source_stage, destination_stage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
+
+		end_single_time_commands(command_buffer);
 	}
 
 	uint32_t find_memory_type(uint32_t type_filter, VkMemoryPropertyFlags properties)
@@ -1373,6 +1548,9 @@ private:
 			vkDestroyImageView(device, swap_chain_image_views[i], nullptr);
 
 		vkDestroySwapchainKHR(device, swap_chain, nullptr);
+
+		vkDestroyImage(device, texture_image, nullptr);
+		vkFreeMemory(device, texture_image_memory, nullptr);
 
 		for (size_t i = 0; i < swap_chain_images.size(); i++)
 		{
